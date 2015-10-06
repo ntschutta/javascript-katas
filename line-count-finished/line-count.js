50bd06999045d12e09502cb4d12b9e8ff7bd8ea1
Nate Schutta <ntschutta@gmail.com>
2015-10-05 20:20:40 -0500

    initial attempt at line count

---
+++ line-count-finished/line-count.js
@@ -0,0 +1,59 @@
+fs = require('fs');
+
+var lineCount = 0;
+fs.readFile('sample1.js', 'utf8', function (err,data) {
+  if (err) {
+    return -1;
+  }
+  var fileAsArray = data.split(/\r\n|\r|\n/);
+  var arrayLength = fileAsArray.length;
+  var nextLine = '';
+  var stringStart = '';
+
+  for (var i = 0; i < arrayLength; i++) {
+    nextLine = fileAsArray[i];
+    stringStart = nextLine.trim().substring(0, 2);
+    console.log(stringStart);
+    if (isSingleLineComment(stringStart)) {
+    } else if (isMultiLineComment(stringStart)){
+      lineCount =+ findCommentEnd(i, fileAsArray);
+    } else {
+      lineCount++;
+    }
+  }
+  console.log('count: ' + lineCount);
+});
+
+function isSingleLineComment (line) {
+  var result = false;
+  if (line === '//') {
+    result = true;
+  }
+  return result;
+}
+
+function isMultiLineComment (line) {
+  var result = false;
+  if (line === '/*') {
+    result = true;
+  }
+  return result;
+}
+
+function findCommentEnd (currentIndex, fileArray) {
+  var notFound = true;
+  var skipCount = 1;
+  while (notFound) {
+    var currentLine = fileArray[currentIndex];
+    currentLine.trim();
+    var endOfLine = currentLine.length;
+    var lineEnd = currentLine.substring(endOfLine - 2, endOfLine);
+    if (lineEnd !== '*/') {
+      skipCount++;
+      currentIndex++;
+    } else {
+      notFound = false;
+    }
+  }
+  return skipCount;
+}
---
+++ line-count-finished/sample1.js
@@ -0,0 +1,18 @@
+
+//here is a comment
+function doFoo() {
+  return 42;
+}
+
+/*another comment*/
+function doBar() {
+  return 13;
+}
+
+/* multi line comment time *
+ *                         *
+ *                         */
+ function doFooBar() {
+   //
+   return 'hello world';
+ }
