91598e162b0b80d1c4c57c3eb5a2a3289ccf70e0
Nate Schutta <ntschutta@gmail.com>
2015-11-22 09:03:17 -0700

    fixed typo

--- roman-numerals-finished/roman-numerals.js
+++ roman-numerals-finished/roman-numerals.js
@@ -15,13 +15,13 @@ function getRoman(place, digit) {
   var hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
   var tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
   var ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  if (place == 1) {
+  if (place === 1) {
     return ones[digit];
   }
-  if (place == 2) {
+  if (place === 2) {
     return tens[digit];
   }
-  if (place == 3) {
+  if (place === 3) {
     return hundreds[digit];
   }
 }
