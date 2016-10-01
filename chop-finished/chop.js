ddc14ecda2346ed5ef2267add8612306c6160aa9
Nate Schutta <ntschutta@gmail.com>
2016-10-01 15:23:56 -0500

    fixed equality operator mistake

--- chop-finished/chop.js
+++ chop-finished/chop.js
@@ -2,7 +2,7 @@ function chop(findMe, arrayOfInts) {
   var startIndex  = 0,
   stopIndex = arrayOfInts.length - 1,
   middle = Math.floor((stopIndex + startIndex)/2);
-  while(arrayOfInts[middle] != findMe && startIndex < stopIndex){
+  while(arrayOfInts[middle] !== findMe && startIndex < stopIndex){
     if (findMe < arrayOfInts[middle]){
       stopIndex = middle - 1;
     } else if (findMe > arrayOfInts[middle]){
